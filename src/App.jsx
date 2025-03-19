f36da9d3ef0fc1e99f6efd1175d01c09b322b4d9
lamh85 <lamh85@gmail.com>
2025-03-18 22:18:01 -0700

    Line parser

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,53 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [rawText, setRawText] = useState('')
+  const [prefix, setPrefix] = useState('')
+
+  const handleTextInput = (event) => {
+    setRawText(event.target.value)
+  }
+
+  const handlePrefixInput = (event) => {
+    setPrefix(event.target.value)
+  }
+
+  const output = () => {
+    const lines = rawText.split('\n').map((line) => {
+      return <p>{`[${prefix}] ${line}`}</p>
+    })
+
+    return lines
+  }
+
+  const containerStyle = {
+    display: 'flex',
+    flexDirection: 'row',
+    width: '100%',
+  }
+
+  const columnStyle = {
+    width: '50%',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+  }
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+    <div style={containerStyle}>
+      <div style={columnStyle}>
+        <h2>Input</h2>
+        <h3>Job ad</h3>
+        <textarea onChange={handleTextInput} value={rawText}></textarea>
+
+        <h3>Config</h3>
+        <div>Prefix:</div>
+        <input onChange={handlePrefixInput} value={prefix}></input>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div style={columnStyle}>
+        <h2>Output</h2>
+        {output()}
+      </div>
+    </div>
   )
 }
 
